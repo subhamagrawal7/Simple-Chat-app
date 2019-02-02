a2c0054f3674b7c059d67cc42fa68aa4398277dc
subhamagrawal7 <subhamagrawal7@gmail.com>
2019-02-02 17:16:06 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express=require('express');
+const app=express();
+
+app.set('view engine','ejs');
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+
+server=app.listen(3000);
+
+const io = require("socket.io")(server)
+
+//listen on every connection
+io.on('connection',(socket)=>{
+    console.log('New user connected');
+
+    //default username
+    socket.username="Anonymus";
+
+    //listen on change username
+    socket.on('change_username',(data)=>{
+        socket.username=data.username;
+    })
+
+    //listen on new message
+    socket.on('new_message', (data)=>{
+        //broadcast the new messages
+        io.sockets.emit('new_message', {message: data.message, username:socket.username});
+    })
+
+    //listen on typing
+    socket.on('typing', (data) => {
+    	socket.broadcast.emit('typing', {username : socket.username})
+    })
+})
---
+++ public/chat.js
@@ -0,0 +1,39 @@
+$(function(){
+    //make connection
+    var socket = io.connect('http://localhost:3000');
+
+    //buttons and inputs
+	var message = $("#message")
+	var username = $("#username")
+	var send_message = $("#send_message")
+	var send_username = $("#send_username")
+	var chatroom = $("#chatroom")
+	var feedback = $("#feedback")
+
+    //Emit a username
+	send_username.click(function(){
+        console.log(username.val())
+		socket.emit('change_username', {username : username.val()})
+    })
+    
+    //Emit message
+	send_message.click(function(){
+		socket.emit('new_message', {message : message.val()})
+	})
+
+	//Listen on new_message
+	socket.on("new_message", (data) => {
+		console.log(data);
+		chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+	})
+
+    //Emit typing
+	message.bind("keypress", () => {
+		socket.emit('typing')
+	})
+
+	//Listen on typing
+	socket.on('typing', (data) => {
+		feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
+	})
+})
